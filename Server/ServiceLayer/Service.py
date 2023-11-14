1c18c844063f2b1269d57010ceb536a55c7db163
kayatsa <kayatsa8@gmail.com>
2023-11-14 12:20:35 +0200

    Service: register support.

--- Server/ServiceLayer/Service.py
+++ Server/ServiceLayer/Service.py
@@ -1,4 +1,5 @@
 from Server.BusinessLayer.Facade import Facade
+from Server.ServiceLayer.Response import Response
 
 
 class Service:
@@ -6,4 +7,9 @@ class Service:
     def __init__(self) -> None:
         self.__facade = Facade()
 
-    
+    def register(self, username: str, password: str) -> Response[bool]:
+        try:
+            self.__facade.register(username, password)
+            return Response.makeValueResponse(True)
+        except Exception as e:
+            return Response.makeErrorResponse(str(e))
