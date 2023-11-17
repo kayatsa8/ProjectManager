7d7296d6b794cb69e2042aeef12b14dd77c37b32
kayatsa <kayatsa8@gmail.com>
2023-11-17 11:19:07 +0200

    temp.

--- Server/main.py
+++
@@ -1,9 +0,0 @@
-from ServiceLayer.Response import Response
-from ServiceLayer.Service import Service
-
-
-service: Service = Service()
-
-response: Response[bool] = service.register("John Thina", "1234")
-
-print(response.getValue())
