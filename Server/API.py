0cbee7b6889600e3d22ac6b33cb89886f0b7a945
kayatsa <kayatsa8@gmail.com>
2023-12-04 12:58:46 +0200

    API: supports user registration.

--- Server/API.py
+++ Server/API.py
@@ -1,11 +1,30 @@
+from typing import Dict
 from flask import Flask, request, jsonify
 
-app = Flask(__name__)
+from ServiceLayer.Service import Service
+from ServiceLayer.Response import Response
+
+app: Flask = Flask(__name__)
+service: Service = Service()
 
 
 @app.route("/")
-def home() -> str:
+def welcome() -> str:
     return "Welcome!"
+
+@app.route("/api/register_user", methods=["POST"])
+def registerUser():
+    userData: Dict[str, str] = request.get_json()
+    response: Response[bool] = service.register(userData["username"], userData["password"])
+
+    return jsonify(response.toDict()), 201
+
+
+
+
+
+
+
 
 
 
