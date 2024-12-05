cea33f00f6754e06a51a0c895f92ff051ee27c4f
kayatsa <kayatsa8@gmail.com>
2024-12-05 19:19:39 +0200

    client/src/useFetch: use fecth custom hook for all methods

--- client/src/useFetch.jsx
+++ client/src/useFetch.jsx
@@ -1,24 +1,58 @@
 import { useState, useEffect } from 'react';
 
 
+/**
+ * 
+ * @param {String} url 
+ * @param {String} method 
+ * @param {any} dataToSend
+ */
+const useFetch = (url, method, dataToSend = {}) => {
+  const [response, setResponse] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+  const body = getRequestBoby(method, dataToSend);
 
+  useEffect(() => {
+    fetch(url, body)
+    .then(res => {
+      if(!res.ok){
+        throw Error("could not send the message to the server");
+      }
 
+      return res.json();
+    })
+    .then(data => {
+      setIsPending(false);
+      setResponse(r => r = data);
+      setError(null);
+    })
+    .catch(err => {
+      setIsPending(false);
+      setError(err.message);
+    })
+  }, [url]);
 
+  return {response, isPending, error};
+};
 
-
-
-
-
-
-
-
-
-
-
-const getPostBoby = (data) => {
+/**
+ * 
+ * @param {String} method 
+ * @param {any} data 
+ * @returns 
+ */
+const getRequestBoby = (method, data) => {
   return {
-    method: "POST",
+    method: method,
     headers: {"Content-Type": "application/json"},
     body: JSON.stringify(data)
   };
 };
+
+
+
+
+
+
+export default useFetch;
