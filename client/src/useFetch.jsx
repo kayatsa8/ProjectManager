728e0589c65e785f59a41f4e441a285f210ea4da
kayatsa <kayatsa8@gmail.com>
2024-12-05 18:52:13 +0200

    client/src/useFetch:  added getPostBody

--- client/src/useFetch.jsx
+++ client/src/useFetch.jsx
@@ -1,47 +1,24 @@
 import { useState, useEffect } from 'react';
 
-const useGet = (url) => {
-  const [data, setData] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const abortController = new AbortController();
-
-    setTimeout(() => {
-      fetch(url, { signal: abortController.signal })
-      .then(res => {
-        if (!res.ok) { // error coming back from server
-          throw Error('could not fetch the data for that resource');
-        } 
-        return res.json();
-      })
-      .then(data => {
-        setIsPending(false);
-        setData(data);
-        setError(null);
-      })
-      .catch(err => {
-        if (err.name === 'AbortError') {
-          console.log('fetch aborted')
-        } else {
-          // auto catches network / connection error
-          setIsPending(false);
-          setError(err.message);
-        }
-      })
-    }, 1000);
-
-    // abort the fetch
-    return () => abortController.abort();
-  }, [url])
-
-  return {data, isPending, error};
-}
 
 
 
 
 
- 
-export default useGet;
+
+
+
+
+
+
+
+
+
+
+const getPostBoby = (data) => {
+  return {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify(data)
+  };
+};
