34c5097446eba8e06aa1de8d6d2d29d4d193c049
kayatsa <kayatsa8@gmail.com>
2024-12-08 16:31:23 +0200

    cleaning

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,37 +8,6 @@ function App() {
   return (
     <Register />
   );
-
-
-
-  // const [url, setUrl] = useState(null);
-  // const {response, isPending, error} = useFetch(url, "PATCH", {username: "admin", password: "admin"});
-
-  // const handleClick = () => {
-  //   setUrl(u => u = "http://localhost:5000//api/log_in");
-  //   console.log(response);
-  // };
-
-
-  // return (
-  //   <>
-  //     <div>
-  //       <button onClick={() => handleClick()}>click</button>
-
-  //       {response &&
-  //         <div>
-  //           name: {response.value.username}
-
-  //           {response.value.projects.map(project => (
-  //             <div key={project}>
-  //               <p>project: {project}</p>
-  //             </div>
-  //           ))}
-  //         </div>
-  //       }
-  //     </div>
-  //   </>
-  // );
 }
 
 export default App
