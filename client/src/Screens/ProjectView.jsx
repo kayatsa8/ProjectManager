9e2325c8d2fac42a86bc754bf3aa230c952593ce
kayatsa <kayatsa8@gmail.com>
2025-01-03 16:14:21 +0200

    client/src/Screens/ProjectView: error handling

--- client/src/Screens/ProjectView.jsx
+++ client/src/Screens/ProjectView.jsx
@@ -1,6 +1,9 @@
 import { useLocation } from 'react-router-dom';
 import { useParams } from "react-router-dom";
 import BackHome from '../Components/BackHome';
+import useFetch from '../useFetch';
+import { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
 
 
 
@@ -8,6 +11,50 @@ import BackHome from '../Components/BackHome';
 const ProjectView = () => {
     const location = useLocation();
     const {projectName} = useParams();
+    const content = {username: location.state.username, projectName: projectName};
+    const {response, isPending, error} = useFetch("http://localhost:5000/api/get_project", "GET", content);
+
+
+    useEffect(() => {
+        if(!error){
+            return;
+        }
+
+        toast.error(error, {
+            position: "bottom-left",
+            autoClose: 4000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "colored",
+        });
+
+    }, [error]);
+
+    useEffect(() => {
+        if(!response){
+            return;
+        }
+
+        if(response.error){
+            toast.error(response.message, {
+                position: "bottom-left",
+                autoClose: 4000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "colored",
+            });
+        }
+
+    }, [response])
+
+
+
 
     return (
         <div>
