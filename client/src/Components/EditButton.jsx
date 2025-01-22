7faca0a0ea131f88e2e6e78824dfd3c2e676a02e
kayatsa <kayatsa8@gmail.com>
2025-01-22 13:26:02 +0200

    Edit Project completed

---
+++ client/src/Components/EditButton.jsx
@@ -0,0 +1,78 @@
+import { useState, useEffect } from "react";
+import useFetch from "../useFetch";
+import { toast } from 'react-toastify';
+
+const EditButton = ({toUrl, content, doBefore, doAfter}) => {
+    const [url ,setUrl] = useState(null);
+    const {response, isPending, error} = useFetch(url, "PATCH", content);
+
+
+    const handleClick = () => {
+        doBefore(content);
+        setUrl(() => toUrl);
+    };
+
+
+    useEffect(() => {
+        if(!error){
+            return;
+        }
+
+        toast.error(error, {
+            position: "bottom-left",
+            autoClose: 4000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "colored",
+        });
+
+        setUrl(() => null);
+    }, [error]);
+
+    useEffect(() => {
+        if(!response){
+            return;
+        }
+
+        if(response.error){
+            toast.error(response.message, {
+                position: "bottom-left",
+                autoClose: 4000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "colored",
+            });
+
+            return;
+        }
+
+        toast.success(`Changes were applied successfully`, {
+            position: "bottom-left",
+            autoClose: 4000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "colored",
+        });               
+
+        setUrl(() => null);
+        doAfter();
+
+    }, [response]);
+
+
+
+    return (
+        <button onClick={() => handleClick()}>Confirm Edit</button>
+    );
+}
+ 
+export default EditButton;
--- client/src/Screens/EditProject.jsx
+++ client/src/Screens/EditProject.jsx
@@ -1,16 +1,16 @@
 import { useState } from "react";
 import { useHistory, useLocation, useParams } from "react-router-dom/cjs/react-router-dom.min";
-import useFetch from "../useFetch";
+import EditButton from "../Components/EditButton";
 
 const EditProject = () => {
     const {projectName} = useParams();
     const location = useLocation();
     const history = useHistory();
     const username = location.state.username;
-    const projects = location.state.projects;
+    let projects = location.state.projects;
     const project = location.state.project;
 
-    const [nameNew, setNameNew] = useState(project.name);
+    const [newName, setNameNew] = useState(project.name);
     const [newDesc, setNewDesc] = useState(project.description);
     const [newLangs, setNewLangs] = useState(() => {
         const temp = project.languages.reduce((str, curr) => str + ", " + curr, "");
@@ -21,10 +21,6 @@ const EditProject = () => {
         return temp.substring(2);
     });
 
-    const [url, setUrl] = useState(null);
-    const [content, setContent] = useState(null);
-    const {response, isPending, error} = useFetch(url, "PATCH", content);
-    
 
 
 
@@ -33,60 +29,80 @@ const EditProject = () => {
         history.push(`/project/${projectName}`, {username: username, projects: projects});
     };
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
 
-        if(nameNew !== project.name){
-            setContent(() => {return {
-                username: username,
-                projectName: projectName,
-                newProjectName: nameNew
-            };});
 
-            setUrl(() => "http://localhost:5000/api/change_project_name");
-        }
 
-        if(newDesc !== project.description){
-            setContent(() => {return {
-                username: username,
-                projectName: nameNew,
-                description: newDesc
-            };});
+    const beforeName = (content) => {
+        content.projectName = projectName;
+        content.newProjectName = newName;
+    };
 
-            setUrl(() => "http://localhost:5000/api/change_project_description");
-        }
+    const afterName = () => {
+        project.name = newName;
 
+        projects = projects.filter((pName) => pName != projectName);
+        projects.push(newName);
+
+        console.log(projects);
+
+        history.push(`/edit/${project.name}`, {
+            username: username,
+            projects: projects,
+            project: project
+        });
+    };
+
+    const beforeDesciption = (content) => {
+        content.description = newDesc;
+    };
+
+    const afterDescription = () => {
+        project.description = newDesc;
+    };
+
+    const beforeLanguages = (content) => {
         let langList = newLangs.split(", ");
-        let toolList = newTools.split(", ");
 
         if(langList.length === 1 && langList[0] === ""){
             langList = [];
         }
 
+        content.languages = langList;
+    };
+
+    const afterLanguages = () => {
+        let langList = newLangs.split(", ");
+
+        if(langList.length === 1 && langList[0] === ""){
+            langList = [];
+        }
+
+        project.languages = langList;
+    };
+
+    const beforeTools = (content) => {
+        let toolList = newTools.split(", ");
+
         if(toolList.length === 1 && toolList[0] === ""){
             toolList = [];
         }
 
-        if(langList !== project.languages){
-            setContent(() => {return {
-                username: username,
-                projectName: nameNew,
-                languages: langList
-            };});
-
-            setUrl(() => "http://localhost:5000/api/change_project_languages");
-        }
-
-        if(toolList !== project.tools){
-            setContent(() => {return {
-                username: username,
-                projectName: nameNew,
-                tools: toolList
-            };});
-
-            setUrl(() => "http://localhost:5000/api/change_project_tools")
-        }
+        content.tools = toolList;
     };
+
+    const afterTools = () => {
+        let toolList = newTools.split(", ");
+
+        if(toolList.length === 1 && toolList[0] === ""){
+            toolList = [];
+        }
+
+        project.tools = toolList;
+    };
+
+
+
+
 
 
     return (
@@ -95,35 +111,70 @@ const EditProject = () => {
 
             <h1>Edit Project</h1>
 
-            <form onSubmit={(event) => handleSubmit(event)}>
-                <label>Title:</label>
-                <input 
-                    type="text"
-                    value={nameNew}
-                    onChange={(event) => {setNameNew(() => event.target.value)}}
-                />
+            <label>Title:</label>
+            <input 
+                type="text"
+                value={newName}
+                onChange={(event) => {setNameNew(() => event.target.value)}}
+            />
+            <EditButton 
+                toUrl="http://localhost:5000/api/change_project_name"
+                content={{
+                    username: username,
+                    projectName: "",
+                    newProjectName: ""
+                }}
+                doBefore={beforeName}
+                doAfter={afterName}
+            />
 
-                <label>Description:</label>
-                <textarea
-                    value={newDesc}
-                    onChange={(event) => {setNewDesc(() => event.target.value)}}
-                />
+            <label>Description:</label>
+            <textarea
+                value={newDesc}
+                onChange={(event) => {setNewDesc(() => event.target.value)}}
+            />
+            <EditButton 
+                toUrl="http://localhost:5000/api/change_project_description"
+                content={{
+                    username: username,
+                    projectName: projectName,
+                    description: ""
+                }}
+                doBefore={beforeDesciption}
+                doAfter={afterDescription}
+            />
 
-                <label>Languages:</label>
-                <textarea
-                    value={newLangs}
-                    onChange={(event) => {setNewLangs(() => event.target.value)}}
-                />
+            <label>Languages:</label>
+            <textarea
+                value={newLangs}
+                onChange={(event) => {setNewLangs(() => event.target.value)}}
+            />
+            <EditButton
+                toUrl="http://localhost:5000/api/change_project_languages"
+                content={{
+                    username: username,
+                    projectName: projectName,
+                    languages: []
+                }}
+                doBefore={beforeLanguages}
+                doAfter={afterLanguages}
+            />
 
-                <label>Tools:</label>
-                <textarea
-                    value={newTools}
-                    onChange={(event) => {setNewTools(() => event.target.value)}}
-                />
-
-                <button>Confirm Edit</button>
-            </form>
-
+            <label>Tools:</label>
+            <textarea
+                value={newTools}
+                onChange={(event) => {setNewTools(() => event.target.value)}}
+            />
+            <EditButton
+                toUrl="http://localhost:5000/api/change_project_tools"
+                content={{
+                    username: username,
+                    projectName: projectName,
+                    tools: []
+                }}
+                doBefore={beforeTools}
+                doAfter={afterTools}
+            />
 
         </div>
     );
